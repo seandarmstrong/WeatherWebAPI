b55135dfea714276ad6a685ee7d1693cbff6152d
sean <armstrong.d.sean@gmail.com>
2018-08-29 22:18:42 -0400

    added second api request for stock symbols

--- index.js
+++ index.js
@@ -1,32 +1,8 @@
-// document.querySelector(".longitude").addEventListener("keyup", getLongitude);
-
-// function getLongitude(event) {
-//   let longitude = event.target.value;
-//   fetch(
-//     `https://forecast.weather.gov/MapClick.php?lat=42.9634&lon=${longitude}&FcstType=json`
-//   )
-//     .then(res => res.json())
-//     .then(req => {
-//       return req;
-//     });
-// }
-
-// document.querySelector(".latitude").addEventListener("keyup", getLatitude);
-
-// function getLatitude(event) {
-//   let latitude = event.target.value;
-//   fetch(
-//     `https://forecast.weather.gov/MapClick.php?lat=${latitude}&lon=${longitude}&FcstType=json`
-//   )
-//     .then(res => res.json())
-//     .then(req => {
-//       return req;
-//     });
-// }
-
 function loadData() {
   fetch(
-    `https://forecast.weather.gov/MapClick.php?lat=42.9634&lon=-85.6681&FcstType=json`
+    `https://forecast.weather.gov/MapClick.php?lat=${
+      document.getElementById("latitude").value
+    }&lon=-${document.getElementById("longitude").value}&FcstType=json`
   )
     .then(response => response.json())
     .then(request => {
@@ -42,19 +18,30 @@ function loadData() {
         `;
         document.querySelector("#data").innerHTML = test;
       }
-      //   timeArray.forEach(item => {
-      //     test += `
-      //         <tr>${item}</tr>
-      //         `;
-      //     document.querySelector("#days").innerHTML = test;
-      //   });
-      //   let test2 = "";
-      //   let textArray = request.data.text;
-      //   textArray.forEach(item => {
-      //     test2 += `
-      //         <tr>${item}</tr>
-      //         `;
-      //     document.querySelector("#forecast").innerHTML = test2;
-      //   });
+    });
+}
+
+function loadStockData() {
+  fetch(
+    `https://api.iextrading.com/1.0/stock/${
+      document.getElementById("stocksymbol").value
+    }/batch?types=quote,news,chart&range=1m&last=10`
+  )
+    .then(response => response.json())
+    .then(request => {
+      let test = "";
+      let stockArray = [
+        request.quote.latestTime,
+        request.quote.symbol,
+        request.quote.previousClose,
+        request.quote.close,
+        request.quote.change
+      ];
+      stockArray.forEach(item => {
+        test += `
+      <td>${item}</td>
+      `;
+        document.querySelector("#stock").innerHTML = test;
+      });
     });
 }
